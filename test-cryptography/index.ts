0dad064e20cf2f8c6ec07f5f3e64aa6bfc59d5f3
olivier <olivier@evaletolab.ch>
2021-02-02 13:11:17 +0100

    chg: simple solution for stateless account content

--- test-cryptography/index.ts
+++ test-cryptography/index.ts
@@ -2,19 +2,46 @@
 // stateless authentification
 import { utils, getPublicKey, sign, verify } from 'noble-ed25519';
 
-const privateKey = utils.randomPrivateKey(); // 32-byte Uint8Array or string.
+const account = 'test@pouet.com:password1';
 
+//
+// SOLUTION A
+import { SHA256 } from 'crypto-js';
+
+const user = account.split(':')[0];
+const hidenUser = SHA256(user);
+const hidenAccount = SHA256(account);
+
+//
+// use the pattern to access user content
+console.log('# simple solution for a stateless readonly account content');
+console.log('/project/users/sha256(user)-sha256(account).json');
+console.log('/project/users/'+hidenUser+'-'+hidenAccount+'.json');
+console.log('# ');
+
+//
+// SOLUTION A
 (async () => {
   //
   // server side operation
+  const privateKey = utils.randomPrivateKey(); // 32-byte Uint8Array or string.
   const publicKey = await getPublicKey(privateKey);
-  const account = 'test@pouet.com:password1';
   const signature = await sign(account, privateKey);  
-  console.log('---server side signed account',signature.toString());
+  console.log('# server side signed account',signature.toString());
 
   //
   // client side, publickey
   const userEntry = 'test@pouet.com:password1';
   const isSigned = await verify(signature, userEntry, publicKey);
-  console.log('---validate user entry ',isSigned);
+  console.log('# validate user entry ',isSigned);
 })();
+
+
+
+
+
+
+
+
+
+
