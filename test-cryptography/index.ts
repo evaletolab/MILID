c29b97a6d04d8cff81274caaa4f76d6661cde044
olivier <olivier@evaletolab.ch>
2021-02-02 10:54:15 +0100

    chg: use specific import instead of *

--- test-cryptography/index.ts
+++ test-cryptography/index.ts
@@ -1,20 +1,20 @@
 //
 // stateless authentification
-import * as ed from 'noble-ed25519';
+import { utils, getPublicKey, sign, verify } from 'noble-ed25519';
 
-const privateKey = ed.utils.randomPrivateKey(); // 32-byte Uint8Array or string.
+const privateKey = utils.randomPrivateKey(); // 32-byte Uint8Array or string.
 
 (async () => {
   //
   // server side operation
-  const publicKey = await ed.getPublicKey(privateKey);
+  const publicKey = await getPublicKey(privateKey);
   const account = 'test@pouet.com:password1';
-  const signature = await ed.sign(account, privateKey);  
+  const signature = await sign(account, privateKey);  
   console.log('---server side signed account',signature.toString());
 
   //
   // client side, publickey
   const userEntry = 'test@pouet.com:password1';
-  const isSigned = await ed.verify(signature, userEntry, publicKey);
+  const isSigned = await verify(signature, userEntry, publicKey);
   console.log('---validate user entry ',isSigned);
 })();
