70f0692517cde97ba3ba3f645438d4885783af43
olivier <olivier@evaletolab.ch>
2021-02-02 10:22:56 +0100

    new: testing state less authentification

---
+++ test-cryptography/index.ts
@@ -0,0 +1,18 @@
+import * as ed from 'noble-ed25519';
+
+const privateKey = ed.utils.randomPrivateKey(); // 32-byte Uint8Array or string.
+
+(async () => {
+  //
+  // server side operation
+  const publicKey = await ed.getPublicKey(privateKey);
+  const account = 'test@pouet.com:password1';
+  const signature = await ed.sign(account, privateKey);  
+  console.log('---server side signed account',signature.toString());
+
+  //
+  // client side, publickey
+  const userEntry = 'test@pouet.com:password1';
+  const isSigned = await ed.verify(signature, userEntry, publicKey);
+  console.log('---validate user entry ',isSigned);
+})();
