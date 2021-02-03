0fa71909a2d79f872b2d1d0f2ea7e019e67d31b5
David Hodgetts <david.demainlalune@gmail.com>
2021-02-03 16:25:28 +0100

    adds milid-frontend

---
+++ milid-frontend/src/views/Admin.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="admin">
+    <h2>Admin</h2>
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .admin {
+    color: red;
+  }
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Landing extends Vue {}
+</script>
---
+++ milid-frontend/src/views/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="landing">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .landing {
+    color: red;
+  }
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Landing extends Vue {}
+</script>
