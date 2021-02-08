874b86a20ffef9cf1871b101979440d62e9f7f8d
David Hodgetts <david.demainlalune@gmail.com>
2021-02-08 17:02:40 +0100

    adds very naive lessonTransformer

--- milid-backend-airtable/src/app.js
+++ milid-backend-airtable/src/app.js
@@ -3,12 +3,30 @@ const extractRawAirtable = require('./extract_raw_airtable');
 
 const { writeFileSync } = require('fs');
 
+
+const { emptyDirSync } = require('fs-extra');
+
+const lessonTransformer = require('./lessonTransformer');
+
 async function main(){
-    const airtableData = await extractRawAirtable();
+    try{
 
-    writeFileSync('./airtable_data.json', JSON.stringify(airtableData, null, 2), {encoding:'utf-8'});
+        const airtableData = await extractRawAirtable();
 
+
+        // transform and massage the data
+        const data = lessonTransformer(airtableData);
+        emptyDirSync('./dist');
+        
+        // write raw airtable structure for reference
+        writeFileSync('./dist/airtable_data.json', JSON.stringify(airtableData, null, 2), { encoding: 'utf-8' });
+        
+        // result to be improved on
+        writeFileSync('./dist/data.json', JSON.stringify(data, null, 2), { encoding: 'utf-8' });
+    }catch(e){
+        console.log("proccess stopped with error", e);
+        process.exit(1);
+    }
 }
-
 
 main();
