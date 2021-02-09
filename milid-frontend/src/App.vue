a9e6e4861e9838ae9cdca0e458dab4b7b9a01e75
olivier <olivier@evaletolab.ch>
2021-02-09 16:57:17 +0100

    chg: first implementation of snap scroll

--- milid-frontend/src/App.vue
+++ milid-frontend/src/App.vue
@@ -5,6 +5,11 @@
 </template>
 
 <style lang="scss">
+
+// 
+// global style
+@import "./app.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
