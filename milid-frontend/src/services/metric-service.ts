691518a89205e39c77cd9668429416a0026ed34c
David Hodgetts <david.demainlalune@gmail.com>
2021-02-24 15:43:25 +0100

    wip

--- milid-frontend/src/services/metric-service.ts
+++ milid-frontend/src/services/metric-service.ts
@@ -2,6 +2,8 @@ import { MILID } from "@/models";
 
 import { Vue } from 'vue-property-decorator';
 
+import { $module } from './module-service';
+
 interface MILIDEvent {
   module: string;
   lesson: string;
@@ -9,6 +11,22 @@ interface MILIDEvent {
   uid: string;
   username: string;
   date: number;
+}
+
+
+interface LessonProgress
+{
+  id: string;
+  state: MILID.LessonState[];
+}
+
+interface ModuleProgress{
+  id: string;
+  lessons: LessonProgress[];
+}
+
+interface ProgressState {
+  modules: ModuleProgress[];
 }
 
 class MetricService {
@@ -21,6 +39,43 @@ class MetricService {
     throw new Error('Not implemented');
   }
 
+  static get localStorageKey(){
+    return "progression";
+  }
+
+  buildEmptryProgressValue(): ProgressState{
+    const result: ProgressState = { modules: []};
+
+    for(const module of $module.modules){
+      const lessons: LessonProgress[] = module.lessons.map(l => {
+        return { 
+          id: l.id, 
+          state: MILID.LessonState.TODO
+        };
+      });
+      const moduleProgress: ModuleProgress = {
+        id: module.id,
+        lessons: lessons
+      }
+      result.modules.push(moduleProgress);
+    }
+
+    return result;
+  }
+
+  computeInitialValue(): ProgressState{
+    let value = this.buildEmptryProgressValue();
+    try{
+      const str = window.localStorage.getItem(MetricService.localStorageKey);
+      if(str){
+        value = JSON.parse(str) as ProgressState;
+      }
+    }catch(e){
+      console.error(e);
+    }
+
+    return value;
+  }
 }
 
 //
