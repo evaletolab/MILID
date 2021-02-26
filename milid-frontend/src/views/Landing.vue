db2fa0f9e2c84d8508209336a34935a1c027681f
olivier <olivier@evaletolab.ch>
2021-02-26 15:37:22 +0100

    chg: update landing

--- milid-frontend/src/views/Landing.vue
+++ milid-frontend/src/views/Landing.vue
@@ -4,30 +4,36 @@
       <div class="left">
         <span class="version">{{ config.version }}</span>
       </div>
-      <div class="">
-        <router-link class="link" to="/home">Home</router-link> 
-        <router-link class="link" to="/About">About</router-link> 
-      </div>
       <div class="right hide-sm">
         <router-link class="link btn" to="/dashboard">Dashboard</router-link>
       </div>
 
     </div>
 
-    <p>create your pseudo</p>
     <section>
       <img src="@/assets/MILID-logo.svg" />
     </section>
     <section>
+      <input v-model="pseudo" placeholder="Choisir un pseudo ..." class="pseudo"/>
+
 
     </section>
+    <section class="bottom " :class="{'open':open,'primary':!open}" @click="onToggle">
+      <MILIDIcons name="back-top" color="white" />
+      <div class="content">
+        <h3>L'expérience est meilleure après installation</h3>
+        <button class="btn tertiary" @click="onInstall">Ajouter à l'écran d'acceuil</button>
+        <div>Test</div>        
+      </div>        
+    </section>
+
   </div>
 </template>
 
 <style lang="scss" scoped>
 
   .landing{
-    background: #8761ff;
+    background: var(--md-theme-default-primary);
     color: white;
     position: fixed;
     width: 100vw;
@@ -65,20 +71,13 @@
       }
     }
 
-    .btn {
-      border-radius: 50px;      
-      background: #fff!important;
-      border-color: #fff;
-      color: #36324a;
-      padding: 15px 20px!important;      
-    }
-
     .version {
       font-weight: 200;
       opacity: .5;
     }
 
     section{
+      padding: 50px 70px;
       max-width: 50%;
       max-height: calc( 100vh - 80px );
       @media (max-width: 376px) {
@@ -87,23 +86,119 @@
       }
     }
   }
+
+  .btn {
+    border-radius: 50px;
+    background: var(--theme-1-tertiary);
+    border: 1px solid var(--theme-1-tertiary);
+    color: var(--theme-1-secondary);
+    text-align: center;
+    padding: 15px 25px !important;
+    margin: auto;
+    font-size: 17px;
+    outline: 0;
+  }
+
+  input.pseudo {
+    line-height: 20px;
+    height: 24px;
+    font-size: 16px;
+    color: #444;
+    padding: 2px 15px;
+    border-radius: 15px;
+    border: none;      
+  }
+
+  section.bottom {
+    color: white;
+    background-color: white;
+    border:1px solid var(--theme-1-tertiary);
+    position: fixed;
+    bottom: 0;
+    left:0;
+    height: calc(100vh - 100px);
+    width: calc( 100vw - 10px );
+    text-align: center;
+    border-radius: 20px 20px 0 0;
+    padding: 0 5px;    
+    transform: translateY(calc(100vh - 120px));
+    transition: all 200ms;      
+    box-shadow: 0 2px 8px 3px rgba(0, 0, 0, 0.2);
+
+
+    &.open {
+      transform: translateY(410px);
+      svg{
+        transform: translateY(-30px) translateX(3px) rotate(180deg) !important;
+      }
+    }
+
+    svg {
+      width: 40px;
+      margin-top: -12px;
+      height: 40px;
+      transform: translateY(-27px) translateX(3px);
+    }
+
+    div.content{    
+      font-size: 14px;
+      font-weight: 200;
+      text-align: left;
+      padding: 5px 20px;              
+      color: #333!important;
+
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+
+      div{
+        width: 100%;
+        text-align: center;
+        padding: 40px;
+      }
+
+      h3{
+        text-align: center;
+        margin-top: -10px;
+        text-transform: uppercase;
+        font-size: 14px;
+        margin-bottom: 30px;    
+        width: 100%;
+      }
+
+
+    }
+
+  }  
+
+
+
 </style>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { $config } from '../services';
 
+import MILIDIcons from '../components/MILIDIcons.vue';
 
 @Component({
-  components: { },
+  components: {
+    MILIDIcons    
+  },
 })
 export default class Landing extends Vue {
-  constructor(){
-    super();
-  }
+  open = false;
+  deferredPrompt: any = {};
+  pseudo = '';
 
   async mounted(){
-    console.log('--- mounted',$config.store.config)
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
   }
 
   get config(){
@@ -112,6 +207,33 @@ export default class Landing extends Vue {
 
   beforeRouteEnter(to: any, from: any, next: any) {
     $config.get().then(next)
+  }
+
+  onToggle(){
+    this.open = !this.open;
+  }
+  
+  onInstall($event) {
+    const deferredPrompt = this.deferredPrompt;
+    // Show the prompt
+    if(!deferredPrompt.prompt) {
+      console.log('---DBG alternative install message',$event);
+      $event.preventDefault();
+      $event.stopPropagation();
+      return;
+    }
+
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt', choiceResult);
+      } else {
+        console.log('User dismissed the A2HS prompt', choiceResult);
+      }
+      this.deferredPrompt = {};
+    });    
   }
 
 }
--- milid-frontend/vue.config.js
+++ milid-frontend/vue.config.js
@@ -7,6 +7,15 @@ module.exports = {
   publicPath : github? '/MILID': (prod? '/':'/'),
   pwa:{
     name:"MILID",
-    themeColor:"#260063",
+    themeColor:"#6900FF",
+    appleMobileWebAppCapable: true,
+    appleMobileWebAppStatusBarStyle:'#6900FF',
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      // swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+
   }
 }
