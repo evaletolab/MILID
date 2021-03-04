2658df2411e3c59345482b5d6b235904acba9d0a
David Hodgetts <david.demainlalune@gmail.com>
2021-03-04 14:20:09 +0100

    changes transparency on DefinitionPopup backdrop

--- milid-frontend/src/components/DefinitionPopup.vue
+++ milid-frontend/src/components/DefinitionPopup.vue
@@ -21,7 +21,7 @@
 
     .backdrop{
         position:fixed;
-        background-color: rgba(0, 0, 0, 0.3);
+        background-color: rgba(0, 0, 0, 0.05);
         width:100%;
         height:100%;
         top:0;
