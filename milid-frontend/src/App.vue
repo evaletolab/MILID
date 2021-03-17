3317c94c0bc07da14c5932ab96cb24691f47efc8
olivier <olivier@evaletolab.ch>
2021-03-17 12:09:37 +0100

    new: detect iOS for custom install

--- milid-frontend/src/App.vue
+++ milid-frontend/src/App.vue
@@ -1,5 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app">  
+    <!-- IOS INSTALL -->
+    <div class="overlay-pane" :class="{'overlay-open':displayIosInstall}">
+      <div class="install-ios">
+        <div class="surface">
+          <div class="cancel material-icons" @click="onClose">close</div>
+          <div class="label">
+            Installez MILID dans votre appareil.<br/> 
+            Appuyez sur l'icône de Partage, puis sélectionnez<br/>
+            <b>Ajouter à l'écran d'accueil.</b>
+          </div>
+          <div class="bottom material-icons" >arrow_downward</div>
+        </div>          
+      </div>
+    </div>
+
     <router-view/>
   </div>
 </template>
@@ -17,17 +32,54 @@
   text-align: center;
   color: #2c3e50;
 }
+</style>
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+  displayIosInstall = false;
+
+  //
+  // Detects if device is on iOS
+  isIos() {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test( userAgent ) ;
+  }
+
+  //
+  // Detects if device is in standalone mode
+  isInStandaloneMode(){ 
+    return ('standalone' in (window as any).navigator) && 
+           ((window as any).navigator.standalone);
+  }
+
+  mounted() {
+    //
+    // delegate browser install Prompt
+    window.addEventListener('beforeinstallprompt', (deferredPrompt) => {
+      // (<any>deferredPrompt).prompt();
+      console.log('PWA browser prompt', deferredPrompt);
+    });
+
+    //
+    // randomly ask for install
+    if (this.isIos() && !this.isInStandaloneMode() && Math.random() > .5) {
+      this.displayIosInstall =  true;
+      setTimeout(()=>{
+        this.displayIosInstall =  false;
+      },10000);
     }
+  }  
+
+  onClose(){
+    this.displayIosInstall = false;
   }
 }
-</style>
+</script>
+
+
+
